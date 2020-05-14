304c8f0d20e61c740bfcf616333b6736e2b9a3a0
Salomao Neto <salomao.neto@objective.com.br>
2020-05-14 07:50:41 -0300

    initial commit

---
+++ src/routes/list-itens/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import { View, Text } from 'react-native';
+
+const ListItens = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+};
+
+export default ListItens;
