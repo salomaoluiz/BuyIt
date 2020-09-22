d3a5a863f089e000053a789c278085f0e1d481e0
salomaoneto <salomao.cruz.15@gmail.com>
2020-09-22 18:27:20 -0300

    [#35] Adicionar banner ad

--- App.tsx
+++ App.tsx
@@ -4,10 +4,12 @@ import { Provider } from 'react-redux';
 import createStore from '@store';
 import RNBootSplash from 'react-native-bootsplash';
 import NotificationCard from '@components/notification-card';
+import firebaseInit from 'src/firebase';
 
 const { store } = createStore();
 const App = () => {
   RNBootSplash.hide();
+  firebaseInit();
 
   return (
     <Provider store={store}>
