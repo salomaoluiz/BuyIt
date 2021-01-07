a55611607cc98378bf4d255d022629a5c79c43ff
salomaoluiz <salomao.cruz.15@gmail.com>
2021-01-07 07:06:31 -0300

    [#72] Removido action de limpeza e add script para rodar Sonar

---
+++ scripts/run-ci.sh
@@ -0,0 +1,13 @@
+if [ -z $PR_ID ]; then echo "Insira um ID de PR"; exit; fi
+
+BRANCH_NAME=pull$PR_ID
+
+git fetch origin pull/$PR_ID/head:$BRANCH_NAME
+git checkout $BRANCH_NAME
+
+yarn
+yarn test
+npx sonar-scanner\
+     -Dsonar.login=$SONAR_TOKEN\
+     -Dsonar.host.url=https://sonarcloud.io\
+     -Dsonar.branch.name=$BRANCH_NAME
